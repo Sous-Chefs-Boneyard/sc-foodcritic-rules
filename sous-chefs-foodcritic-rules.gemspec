8f1a945748534190b611fa20efbddbdb9efce39f
Dan Webb <dan.webb@damacus.io>
2017-05-25 22:50:11 -0500

    Move sample_cookbooks to fixtures to make it work with rspec-command

--- sous-chefs-foodcritic-rules.gemspec
+++ sous-chefs-foodcritic-rules.gemspec
@@ -19,8 +19,11 @@ Gem::Specification.new do |spec|
   spec.required_ruby_version = ">= 2.3.0"
 
   spec.add_dependency "foodcritic"
+  spec.add_dependency "rspec-command"
+  spec.add_dependency "simplecov"
 
   spec.add_development_dependency "bundler"
   spec.add_development_dependency "rake"
   spec.add_development_dependency "rspec"
+
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,22 +1,115 @@
 require "rspec"
 require "foodcritic"
+require "shellwords"
+require "rspec_command"
+require "simplecov"
 
-PROJECT_ROOT = File.expand_path(File.join(File.dirname(__FILE__), "../"))
-
-def foodcritic_run(rule_name)
-  fc = FoodCritic::Linter.new
-
-  opts = {
-    include_rules: [File.join(PROJECT_ROOT, "lib/foodcritic/rules")],
-    # tags: ["sous-chefs"],
-    # tags: ["sc002"],
-    # cookbook_paths: File.join(PROJECT_ROOT, 'sample_cookbooks', rule_name.downcase)
-    cookbook_paths: File.join(PROJECT_ROOT, "sample_cookbooks", rule_name.downcase),
-  }
-
-  fc.check(opts)
+SimpleCov.start do
+  add_filter "/spec/"
 end
 
-def warnings(fc_run)
-  fc_run.warnings.collect { |w| File.basename(w.match[:filename]) }.uniq
+module FunctionalHelpers
+  extend RSpec::Matchers::DSL
+
+  matcher :violate_rule do |rule_id = nil|
+    match do |cmd|
+      if location
+        cmd.stdout =~ /^#{expected}:.*: \.\/#{location}/
+      else
+        cmd.stdout =~ /^#{expected}:/
+      end
+    end
+    chain :in, :location
+    failure_message do |cmd|
+      "expected a violation of rule #{expected}#{location && " in #{location}"}, output was:\n#{cmd.stdout}"
+    end
+    failure_message_when_negated do |cmd|
+      "expected no violation of rule #{expected}#{location && " in #{location}"}, output was:\n#{cmd.stdout}"
+    end
+    # Override the default behavior from RSpec for the expected value, use
+    # define_method insetad of def so we can see the _rule_id variable in closure.
+    define_method(:expected) do
+      # Fill in the top-level example group description as the rule ID if not specified.
+      rule_id || method_missing(:class).parent_groups.last.description
+    end
+  end
+
+  def foodcritic_command(*args)
+    output = StringIO.new
+    error = StringIO.new
+    begin
+      # Don't use the block form of chdir because for some reason it can't be
+      # nested.
+      cwd = Dir.pwd
+      Dir.chdir(temp_path)
+      $stderr = error
+      exitstatus = FoodCritic::CommandLine.main(args, output)
+    ensure
+      $stderr = STDERR
+      Dir.chdir(cwd)
+    end
+    RSpecCommand::OutputString.new(output.string, error.string).tap do |out|
+      out.define_singleton_method(:exitstatus) { exitstatus }
+    end
+  end
+
+  module ClassMethods
+    def foodcritic_command(*args)
+      metadata[:foodcritic_command] = true
+      subject do |example|
+        foodcritic_command(*args)
+      end
+    end
+
+    def attributes_file(*args, &block)
+      file("attributes/default.rb", *args, &block)
+    end
+
+    def resource_file(*args, &block)
+      file("resources/my_resource.rb", *args, &block)
+    end
+
+    def library_file(*args, &block)
+      file("libraries/helper.rb", *args, &block)
+    end
+
+    def recipe_file(*args, &block)
+      file("recipes/default.rb", *args, &block)
+    end
+
+    def metadata_file(*args, &block)
+      file("metadata.rb", *args, &block)
+    end
+
+    def included(klass)
+      super
+      klass.extend ClassMethods
+      # Set a default subject command, can be overridden if needed.
+      klass.foodcritic_command("--no-progress", ".")
+    end
+  end
+
+  extend ClassMethods
 end
+
+RSpec.configure do |config|
+  # Basic configuraiton
+  config.run_all_when_everything_filtered = true
+  config.filter_run(:focus) unless ENV["CI"]
+  config.order = "random"
+
+  # Set some metadata based on test folders.
+  # config.define_derived_metadata(file_path: %r{spec/unit}) do |metadata|
+  #   metadata[:unit] = true
+  # end
+  # config.define_derived_metadata(file_path: %r{spec/functional}) do |metadata|
+  #   metadata[:functional] = true
+  # end
+  # config.define_derived_metadata(file_path: %r{spec/regression}) do |metadata|
+  #   metadata[:regression] = true
+  # end
+  config.include RSpecCommand
+  config.include FunctionalHelpers#, functional: true
+end
+
+# require_relative "../lib/foodcritic/rules"
